d39d1776dbd4819f9046f9742e46bd26c2260cbf
Josecarlosgp <56558929+Josecarlosgp@users.noreply.github.com>
2021-05-12 09:45:45 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -2,7 +2,6 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
 	let tabla = document.querySelector(".tabla");
-	alert(tabla);
 	for(let i=0; i<libros.length; i++)
 		tabla.innerHTML += "<div class = 'fila'>" +  libros[i].textContent + "</div>"
 		// capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
